f6f0e464a7f5e8b438b003caf5fd98d5d7404655
lordslair <5469390+lordslair@users.noreply.github.com>
2021-03-01 17:41:43 +0100

    Modified for new path & symlink

--- ng-cli/entrypoint.sh
+++ ng-cli/entrypoint.sh
@@ -2,6 +2,16 @@
 OK="\t[\e[92m✓\e[0m]"
 KO="\t[\e[91m✗\e[0m]"
 
+DAY=`date +"%d"`
+FULLPATH="$OUTPUT_PATH/$OUTPUT_FOLDER"
+
+echo "path:$OUTPUT_PATH"
+echo "folder:$OUTPUT_FOLDER"
+echo "fullpath:$FULLPATH"
+echo "daily:$FULLPATH.$DAY"
+echo "build:$BUILD_TYPE"
+echo '-----'
+
 if [ ! -z $GIT_REPO ]
 then
     echo -n `date +"%F %X"` "Cloning"
@@ -37,16 +47,29 @@ else
     echo -e $KO
 fi
 
-if [ -d $OUTPUT_PATH ]
+if [ -d $FULLPATH ]
 then
     echo -n `date +"%F %X"` "Building"
 
     if [ $QUIET='True' ]
     then
-        cd /app && ng build --configuration=$BUILD_TYPE --outputPath=$OUTPUT_PATH >/dev/null 2>&1
+        cd /app && ng build --configuration=$BUILD_TYPE --outputPath="$FULLPATH.$DAY" >/dev/null 2>&1
     else
-        cd /app && ng build --configuration=$BUILD_TYPE --outputPath=$OUTPUT_PATH
+        cd /app && ng build --configuration=$BUILD_TYPE --outputPath="$FULLPATH.$DAY"
     fi
+
+    if [ $? -eq 0 ]
+    then
+        echo -e $OK
+    else
+        echo -e $KO
+    fi
+
+    # We remove the old symlink & We create the new symlink
+    echo -n `date +"%F %X"` "Linking"
+    cd $OUTPUT_PATH
+    rm -f "$OUTPUT_FOLDER"
+    ln -s "$OUTPUT_FOLDER.$DAY" $OUTPUT_FOLDER
 
     if [ $? -eq 0 ]
     then
